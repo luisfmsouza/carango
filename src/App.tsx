041ba6b21b83e038926e63c0269da6210c4e3da9
Luís Felipe Souza <luisfmsouza@gmail.com>
2019-10-04 18:23:54 -0300

    Crete page structure within form and content
    Add:
    - CarNext logo file;
    - Arrow right icon svg;

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,70 @@
 import React, { FC } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import logo from "./assets/images/car-next-logo.png";
+import { ReactComponent as IconRight } from "./assets/icons/arrow-right.svg";
 
 export const App: FC = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.tsx</code> and save to reload.
-    </p>
-  </div>
+  <>
+    <header>
+      <a href="#">
+        <img src={logo} alt="CarNext logo" title="CarNext logo" />
+      </a>
+    </header>
+    <section>
+      <h1>Get a free year of driving!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In erat eros,
+        finibus ac ullamcorper sit amet, tempus quis lacus. Quisque eleifend
+        semper pretium. Sed rutrum odio lectus, id maximus nunc rhoncus sed.
+        Praesent quis suscipit dui. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer orci dui, rutrum eu feugiat lobortis, tristique
+        sollicitudin est. Nulla tincidunt semper nibh at pulvinar. Etiam tempor
+        quam vitae neque ornare efficitur. Morbi id egestas est, at viverra
+        orci. Fusce eleifend placerat ligula, eget viverra lectus facilisis
+        vitae. Praesent placerat tortor a mi laoreet, quis lobortis nunc
+        eleifend.
+      </p>
+    </section>
+    <section>
+      <h2>Get a car leasing offer</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In erat eros,
+        finibus ac ullamcorper sit amet, tempus quis lacus.
+      </p>
+      <form>
+        <label htmlFor="form--first-name">First name</label>
+        <input
+          id="form--first-name"
+          placeholder="Enter first name"
+          type="text"
+        />
+        <label htmlFor="form--surname">Surname</label>
+        <input id="form--surname" placeholder="Enter surname" type="text" />
+        <label htmlFor="form--email">Email address</label>
+        <input
+          id="form--email"
+          placeholder="Enter your email address"
+          type="email"
+        />
+        <label htmlFor="form--phone">Phone number</label>
+        <input
+          id="form--phone"
+          placeholder="Enter your phone number"
+          type="tel"
+        />
+        <label htmlFor="form--voucher">Voucher code</label>
+        <input
+          id="form--voucher"
+          placeholder="Enter your voucher code"
+          type="number"
+        />
+        <input id="form--policy" type="checkbox" />
+        <label htmlFor="form--policy">Accept Privacy Settlement</label>
+        <button type="submit">
+          Get Offer
+          <IconRight />
+        </button>
+      </form>
+    </section>
+  </>
 );
